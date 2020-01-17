526afa70edd367c2fe4751a5ad644c5da89a6489
Walid Beidas <walidbeidas@Walids-MacBook-Air.local>
2020-01-17 10:38:58 -0800

    doing messages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [messages, setMessages] = useState([])
+  console.log(messages)
+  return <main>
+
+    <header> 
+      <img className="logo"
+        alt="logo"
+        src="" 
+      />
+      Chatter
+    </header>
+
+    <div className="messages"></div>
+    {messages.map((m,i)=>{
+      return <div key={i} className= "message">{m}</div>
+    })}
+    
+    <TextInput onSend={(text)=> {
+      setMessages([text,...messages])
+    }} />
+    
+  </main>
+}
+
+
+function TextInput(props){
+  var [text, setText] = useState('') 
+  // normal js comment
+  return <div className="text-input-wrap">
+    <input value={text} className="text-input"
+      placeholder="write your message"
+      onChange={e=> setText(e.target.value)}
+    />
+    <button onClick={()=> {
+      if (text) {
+      props.onSend(text)
+      }
+      setText('')
+    }} className="button">
+      {/*disabled={!text}*/}
+      SEND
+    </button>
+  </div>
 }
 
 export default App;
