8f480aa7886162b32a0d08368f56c5342cc832d2
Walid Beidas <walidbeidas@Walids-MacBook-Air.local>
2020-01-22 14:29:04 -0800

    newest update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
+import {db} from './db'
+import NamePicker from './namePicker'
 
 function App() {
   const [messages, setMessages] = useState([])
-  console.log(messages)
+  const [name, setName] = useState('')
+  useEffect(()=>{
+    db.listen({
+      receive: m=> {
+        setMessages(current=> [m, ...current])
+      },
+      remove: id=> {
+        setMessages(current=> [...current].filter(m => m.id !== id))
+      },
+    })
+  },[])
   return <main>
 
-    <header> 
-      <img className="logo"
-        alt="logo"
-        src="" 
-      />
-      Chatter
+    <header>
+      <div className="logo-wrap">
+        <img className="logo"
+          alt="logo"
+          src="https://www.pngjoy.com/pngm/83/1773038_cool-png-cool-logos-transparent-png.png" 
+        />
+        Chatter
+      </div>
+      <NamePicker onSave={setName} />
     </header>
 
-    <div className="messages"></div>
-    {messages.map((m,i)=>{
-      return <div key={i} className= "message">{m}</div>
-    })}
-    
+    <div className="messages">
+      {messages.map((m,i)=>{
+        return <div key={i} className="message-wrap">
+          <div className="message">{m.text}</div>
+        </div>
+      })}
+    </div>
+
     <TextInput onSend={(text)=> {
-      setMessages([text,...messages])
+      db.send({
+        text, name, ts: new Date(),
+      })
     }} />
     
   </main>
@@ -31,20 +51,31 @@ function TextInput(props){
   var [text, setText] = useState('') 
   // normal js comment
   return <div className="text-input-wrap">
-    <input value={text} className="text-input"
+    <input 
+      value={text} 
+      className="text-input"
       placeholder="write your message"
       onChange={e=> setText(e.target.value)}
+      onKeyPress={e=> {
+        if(e.key==='Enter') {
+          if(text) props.onSend(text)
+          setText('')
+        }
+      }}
     />
     <button onClick={()=> {
-      if (text) {
-      props.onSend(text)
-      }
+      if(text) props.onSend(text)
       setText('')
-    }} className="button">
-      {/*disabled={!text}*/}
+    }} className="button"
+      disabled={!text}>
       SEND
     </button>
   </div>
 }
 
-export default App;
+export default App
+
+
+
+
+  
